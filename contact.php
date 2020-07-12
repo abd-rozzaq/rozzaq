9b94e1ae18b6827cfb1a56f9436ee5c1ed9dcc5c
rozzaq <64617660+abd-rozzaq@users.noreply.github.com>
2020-07-12 14:02:31 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -16,10 +16,10 @@ $email_body = "Name: $name.\n" .
 
 $to = "contact@rozzaq.com";
 $headers = "From: $email_from \r\n";
-$headers = "Reply-To: $email \r\n";
+$headers .= "Reply-To: $email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
-header("location: index.html");
+header("location: index.hmtl");
 
 ?>
