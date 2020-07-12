9c9d233d3130dcff8913712bf89572923dfd44b9
rozzaq <64617660+abd-rozzaq@users.noreply.github.com>
2020-07-12 13:41:38 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$designtype = $_POST['designtype'];
+$priority = $_POST['priority'];
+$message = $_POST['message'];
+
+$email_from = 'Rozzaq';
+$email_subject = 'New Message From Rozzaq Contact';
+$email_body = "Name: $name.\n" .
+    "Email: $email.\n" .
+    "Designtype: $designtype.\n" .
+    "Priority: $priority.\n" .
+    "Message: $message.\n";
+
+$to = "contact@rozzaq.com";
+$headers = "From: $email_from \r\n";
+$headers = "Reply-To: $email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("location: front-page.php");
